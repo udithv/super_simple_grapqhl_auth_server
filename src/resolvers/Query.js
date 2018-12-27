97542a9884eeec592a91c02be46fb99da34d2084
udith victor <udithv01@gmail.com>
2018-12-27 21:29:17 +0530

    added filtering using where prisma

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,18 @@
 const { getUserId } = require('../utils')
 
-function feed(root, args, context, info) {
-    return context.prisma.links()
+async function feed(root, args, context, info) {
+    const where = args.filter ? {
+        OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter },
+        ],
+    } : {}
+    
+    const links = await context.prisma.links({
+        where
+    })
+
+    return links
 }
 
 function votes(root, args, context, info) {
